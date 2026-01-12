8eda4e728e351bfffc79b22b2b1c3a09c48f5b5f
Julio Medina <developer.medina@outlook.com>
2026-01-11 23:36:46 -0300

    Testing mobile download

--- script.js
+++ script.js
@@ -303,11 +303,3 @@ typingEffect();
 
 // Funcao para visualizacao de curriculo em celulares (nao da pra fazer download)
 
-const isMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
-const cvLink = document.getElementById('cv-link');
-
-if (isMobile) {
-    cvLink.removeAttribute('download');
-    cvLink.setAttribute('target', '_blank');
-    cvLink.textContent = 'Abrir CV (PDF)';
-}
